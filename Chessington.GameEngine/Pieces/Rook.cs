aff04cd3b6b1aa2ea686861ccb3ff673004cae47
Arina Demidova <arina.demidova@softwire.com>
2022-11-23 14:41:05 +0000

    Test 3 passes successfully: rook can move laterally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,34 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> possibleMoves = new List<Square>();
+
+            Player CurrentPlayer = this.Player == Player.White ? Player.White : Player.Black;
+            var currentSquare = board.FindPiece(this);
+
+            for (var i = currentSquare.Row; i < 8; i++)
+            {
+                possibleMoves.Add(Square.At(i, currentSquare.Col));
+            }
+
+            for (var i = currentSquare.Row; i >= 0; i--)
+            {
+                possibleMoves.Add(Square.At(i, currentSquare.Col));
+            }
+
+            for (var i = currentSquare.Col; i < 8; i++)
+            {
+                possibleMoves.Add(Square.At(currentSquare.Row, i));
+            }
+
+            for (var i = currentSquare.Col; i >= 0; i--)
+            {
+                possibleMoves.Add(Square.At(currentSquare.Row, i));
+            }
+
+            possibleMoves.RemoveAll(s => s == currentSquare);
+
+            return possibleMoves;
         }
     }
 }
