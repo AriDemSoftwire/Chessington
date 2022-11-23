7eb3ea158834c635a9d68226d0f5b48b96843cd4
Arina Demidova <arina.demidova@softwire.com>
2022-11-23 15:01:58 +0000

    Test 4 passes successfully: bishop can move diagonally

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,59 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+
+            List<Square> possibleMoves = new List<Square>();
+
+            Player CurrentPlayer = this.Player == Player.White ? Player.White : Player.Black;
+            var currentSquare = board.FindPiece(this);
+
+            for (int i = currentSquare.Row; i < 8; i++)
+            {
+                for (int j = currentSquare.Col; j < 8; j++)
+                {
+                    if (Math.Abs(currentSquare.Row - i) == Math.Abs(currentSquare.Col - j))
+                    {
+                        possibleMoves.Add(Square.At(i, j));
+                    }
+                }
+            }
+
+            for (int i = currentSquare.Row; i < 8; i++)
+            {
+                for (int j = currentSquare.Col; j >= 0; j--)
+                {
+                    if (Math.Abs(currentSquare.Row - i) == Math.Abs(currentSquare.Col - j))
+                    {
+                        possibleMoves.Add(Square.At(i, j));
+                    }
+                }
+            }
+
+            for (int i = currentSquare.Row; i >= 0; i--)
+            {
+                for (int j = currentSquare.Col; j >= 0; j--)
+                {
+                    if (Math.Abs(currentSquare.Row - i) == Math.Abs(currentSquare.Col - j))
+                    {
+                        possibleMoves.Add(Square.At(i, j));
+                    }
+                }
+            }
+
+            for (int i = currentSquare.Row; i >= 0; i--)
+            {
+                for (int j = currentSquare.Col; j < 8; j++)
+                {
+                    if (Math.Abs(currentSquare.Row - i) == Math.Abs(currentSquare.Col - j))
+                    {
+                        possibleMoves.Add(Square.At(i, j));
+                    }
+                }
+            }
+
+            possibleMoves.RemoveAll(s => s == currentSquare);
+
+            return possibleMoves;
         }
     }
 }
