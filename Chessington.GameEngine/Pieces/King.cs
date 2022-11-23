1502a6149ec0d60e11e0b8ba4d94d9347ead6af5
Arina Demidova <arina.demidova@softwire.com>
2022-11-23 15:22:24 +0000

    Test 7 passes successfully: king can move

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +12,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> possibleMoves = new List<Square>();
+
+            var currentSquare = board.FindPiece(this);
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Math.Abs(currentSquare.Row - i) <= 1 && Math.Abs(currentSquare.Col - j) <= 1)
+                    {
+                        possibleMoves.Add(Square.At(i, j));
+                    }
+                }
+            }
+
+            possibleMoves.RemoveAll(s => s == currentSquare);
+
+            return possibleMoves;
         }
     }
 }
