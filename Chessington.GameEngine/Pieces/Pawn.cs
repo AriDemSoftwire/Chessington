c7202164c0107d48871c418a227f146d95c33acd
Arina Demidova <arina.demidova@softwire.com>
2022-11-23 14:14:03 +0000

    Test 1 passes successfully: pawns can move one square up

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,16 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Chessington.GameEngine;
+using Chessington.GameEngine.Pieces;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> possibleMoves = new List<Square>();
+
+            Player CurrentPlayer = this.Player == Player.White ? Player.White : Player.Black;
+            var currentSquare = board.FindPiece(this);
+
+            if (CurrentPlayer == Player.White)
+            {
+                Console.WriteLine("white");
+                var squareUp = Square.At(currentSquare.Row - 1, currentSquare.Col);
+                if (board.isSquareEmpty(squareUp) == true)
+                {
+                    possibleMoves.Add(squareUp);
+                }
+            }
+
+            if (CurrentPlayer == Player.Black)
+            {
+                Console.WriteLine("black");
+                var squareDown = Square.At(currentSquare.Row + 1, currentSquare.Col);
+                if (board.isSquareEmpty(squareDown) == true)
+                {
+                    possibleMoves.Add(squareDown);
+                }
+            }
+
+            return possibleMoves;
         }
     }
 }
+
